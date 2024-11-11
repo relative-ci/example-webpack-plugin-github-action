e57a74d33b22a878d3c4c2a28470f60b162860ca
Viorel Cojocaru <vio@beanon.com>
2024-11-11 18:06:21 +0100

    fix: Remove obsolete files

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-CI=true RELATIVE_CI_ENDPOINT="http://localhost:3000" RELATIVE_CI_COMMIT=abc123 RELATIVE_CI_COMMIT_MESSAGE="Really: should work" DEBUG=relative-ci:* RELATIVE_CI_KEY=abc123 RELATIVE_CI_SLUG=abc/123 npm run build
